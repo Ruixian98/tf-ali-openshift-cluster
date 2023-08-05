64141d9403b19d94e99501a7d3adf5a3a6bdd1af
rli <rli@humanlongevity.com>
2023-08-05 13:03:57 +0800

    init

--- variables.tf
+++ variables.tf
@@ -6,18 +6,18 @@ variable "region" {
     default = "cn-hongkong"
 }
 variable "access_key" {
-    default = "LTAI5tJ2ChAmbGpvAWcEhaBc"
+    default = "LTAI*****haBc"
 }
 variable "secret_key" {
-    default = "Chn8cYaiTUMNmfic28VLGcNuScyVrI"
+    default = "Chn8*****yVrI"
 }
 #启动用于执行自动化部署脚本的服务器所在的vpc
 variable "launcher_vpc_id" {
-    default = "vpc-j6cief7zf2wni5l52yf5y"
+    default = "vpc-j6ci*****yf5y"
 }
 #启动用于执行自动化部署脚本的服务器所在的交换机
 variable "launcher_vswitch_id" {
-    default = "vsw-j6cr48zg87vpjmrunz0n6"
+    default = "vsw-j6cr*****z0n6"
 }
 #用于执行自动化部署脚本的服务器的ssh登录密码，用户为root
 variable "ssh_password" {
@@ -29,5 +29,5 @@ variable "openshift_cluster_name" {
 }
 #openshift集群访问地址使用的域名，必须在上面AKSK所在的账户中
 variable "base_domain" {
-    default = "uxian.online"
+    default = "*****.*****"
 }
