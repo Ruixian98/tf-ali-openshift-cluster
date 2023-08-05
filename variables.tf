c2236c08f97b124c45e6af4c525d3c95a9402f63
rli <rli@humanlongevity.com>
2023-08-05 13:52:48 +0800

    update readme

--- variables.tf
+++ variables.tf
@@ -6,18 +6,18 @@ variable "region" {
     default = "cn-hongkong"
 }
 variable "access_key" {
-    default = "LTAI5tRyNnEpyL4tRNQbRYMp"
+    default = "LTAI******RYMp"
 }
 variable "secret_key" {
-    default = "Jg9QKUfljr2QH7Z0FGfWqvaNYsPx8G"
+    default = "Jg9Q******Px8G"
 }
 #启动用于执行自动化部署脚本的服务器所在的vpc
 variable "launcher_vpc_id" {
-    default = "vpc-j6cief7zf2wni5l52yf5y"
+    default = "vpc-j6ci******yf5y"
 }
 #启动用于执行自动化部署脚本的服务器所在的交换机
 variable "launcher_vswitch_id" {
-    default = "vsw-j6cr48zg87vpjmrunz0n6"
+    default = "vsw-j6cr******z0n6"
 }
 #用于执行自动化部署脚本的服务器的ssh登录密码，用户为root
 variable "ssh_password" {
@@ -25,9 +25,9 @@ variable "ssh_password" {
 }
 #openshift集群访问地址使用的域名，必须在上面AKSK所在的账户中
 variable "base_domain" {
-    default = "uxian.online"
+    default = "******.******"
 }
-#openshift集群名称，该名称将用于最后集群提供公网访问的域名地址，比如test.uxian.online，*.test.uxian.online 等一系列域名
+#openshift集群名称，该名称将用于最后集群提供公网访问的域名地址，比如test.******.*******.test.******.****** 等一系列域名
 variable "openshift_cluster_name" {
     default = "test"
 }
